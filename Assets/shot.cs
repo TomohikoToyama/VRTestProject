7d185bb49b6c7617763b60aebf58cda6a82b5ce3
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-06-13 00:01:23 +0900

    ショット用のスクリプト追加

---
+++ Assets/shot.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour {
+    float limit = 10f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float translation = Time.deltaTime * 10;
+        transform.Translate(0, 0, translation);
+        if (limit - Time.deltaTime > 0)
+        {
+            Destroy(this);
+        }
+
+    }
+}
