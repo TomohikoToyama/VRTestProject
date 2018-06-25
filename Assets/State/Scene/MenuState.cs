de51205dae798daabc4694591cd7b30be624ba04
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-06-25 23:56:18 +0900

    MenuState追加、プレイヤー用のStateフォルダ追加

---
+++ Assets/State/Scene/MenuState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace VR
+{
+    public class MenuState : IState
+    {
+        private GameStateManager manager;
+        public MenuState(GameStateManager GSM)
+        {
+            manager = GSM;
+            Time.timeScale = 0;
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public void StateUpdate()
+        {
+
+        }
+    }
+}
