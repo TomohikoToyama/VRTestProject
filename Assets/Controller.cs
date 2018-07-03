5d2950493d626b563621d666c41e81fa01f6ce74
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-07-03 23:59:36 +0900

    弾のメソッドをプレイヤーステータスに移植

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -2,94 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+namespace VR
+{
+    public class Controller : MonoBehaviour
+    {
 
-public class Controller : MonoBehaviour{
+        [SerializeField]
+        public GameObject shot;
 
-    [SerializeField]
-    GameObject prefab;
+        [SerializeField]
+        GameObject missile;
 
-    [SerializeField]
-    GameObject missile;
+        [SerializeField]
+        PlayerStatusController PSC;
 
-    // 弾丸発射点
-    [SerializeField]
-    public Transform muzzle;
-    public float cool_time = 0.05f;
-    [SerializeField]
-    GameObject player;
-    GameObject _parent;
-    bool shotLimit = false;
+        // 弾丸発射点
+        [SerializeField]
+        public Transform muzzle;
+        public float cool_time = 0.05f;
+        [SerializeField]
+        public GameObject player;
+        GameObject _parent;
+        bool shotLimit = false;
 
-    // Use this for initialization
-    void Start () {
-        _parent = transform.root.gameObject;
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        var trackedObject = GetComponent<SteamVR_TrackedObject>();
-        var device = SteamVR_Controller.Input((int)trackedObject.index);
-
-        
-       
-        //右キー
-        if ( device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        // Use this for initialization
+        void Start()
         {
-            device.TriggerHapticPulse(3500);
-            prefab = Instantiate(prefab, muzzle.transform.position, Quaternion.identity);
-            prefab.transform.position = muzzle.position;
-            prefab.transform.eulerAngles = player.transform.eulerAngles;
-            prefab.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * 3f);
+            _parent = transform.root.gameObject;
 
-           // Instantiate(prefab, player.transform.position, Quaternion.identity);
-            Debug.Log("トリガーを深く引いた");
         }
 
-        if ( device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        // Update is called once per frame
+        void Update()
         {
-            this.gameObject.transform.Translate(0, 0, -1f);
-            Debug.Log("トリガーを離した");
-        }
+            var trackedObject = GetComponent<SteamVR_TrackedObject>();
+            var device = SteamVR_Controller.Input((int)trackedObject.index);
 
-        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
-        {
 
-            _parent.transform.Translate(0.05f * device.GetAxis().x, 0, 0.05f * device.GetAxis().y);
-            Debug.Log("タッチパッドをクリックしている");
-        }
-     
-      
-        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            Debug.Log("タッチパッドを離した");
-        }
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
-        {
-          //  if (!PlayerManager.(true)) {
-                
+
+            //右キー
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+
+                 PSC.ShotBullet();
+               
+
+            }
+
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                this.gameObject.transform.Translate(0, 0, -1f);
+                Debug.Log("トリガーを離した");
+            }
+
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+
+                _parent.transform.Translate(0.05f * device.GetAxis().x, 0, 0.05f * device.GetAxis().y);
+                Debug.Log("タッチパッドをクリックしている");
+            }
+
+
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                Debug.Log("タッチパッドを離した");
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+            {
+                //  if (!PlayerManager.(true)) {
+
                 Debug.Log("メニューボタンをクリックした");
-           // }
-        }
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-        {
-            this.transform.root.position = new Vector3(5, 1, 5);
-            Debug.Log("グリップボタンをクリックした");
-        }
+                // }
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            {
+                this.transform.root.position = new Vector3(5, 1, 5);
+                Debug.Log("グリップボタンをクリックした");
+            }
 
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
-        {
-            //Debug.Log("トリガーを浅く引いている");
-        }
-        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
-        {
-            //Debug.Log("トリガーを深く引いている");
-        }
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            _parent.transform.Translate(0.025f * device.GetAxis().x, 0, 0.025f * device.GetAxis().y);
-            //Debug.Log("タッチパッドに触っている");
-        }
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                //Debug.Log("トリガーを浅く引いている");
+            }
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                //Debug.Log("トリガーを深く引いている");
+            }
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                _parent.transform.Translate(0.025f * device.GetAxis().x, 0, 0.025f * device.GetAxis().y);
+                //Debug.Log("タッチパッドに触っている");
+            }
 
+        }
     }
 }
--- Assets/Script/Controller/PlayerStatusController.cs
+++ Assets/Script/Controller/PlayerStatusController.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace VR
 {
     public class PlayerStatusController : MonoBehaviour
     {
+        [SerializeField]
+        public GameObject shot;
+
+        // 弾丸発射点
+        [SerializeField]
+        public Transform muzzle;
+
+        [SerializeField]
+        public GameObject player;
+
+        Controller Con;
         public int HP = 3;
         public int MaxHP = 5;
         public int missile = 5;             //ミサイルストック数
@@ -19,7 +31,7 @@ namespace VR
         // Use this for initialization
         void Start()
         {
-
+            
         }
 
         // Update is called once per frame
@@ -27,6 +39,19 @@ namespace VR
         {
 
         }
+
+        //弾を撃つ
+        public void ShotBullet()
+        {
+
+            shot = Instantiate(shot, muzzle.transform.position, Quaternion.identity);
+            shot.transform.position = muzzle.position;
+            shot.transform.eulerAngles = player.transform.eulerAngles;
+            shot.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * 3f);
+            // Instantiate(prefab, player.transform.position, Quaternion.identity);
+            Debug.Log("トリガーを深く引いた");
+        }
+
 
     }
 }
