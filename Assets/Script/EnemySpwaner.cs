82df97406544b97f6ad92c16c1b20913d3a17424
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-07-13 23:59:50 +0900

    敵生成テスト

--- Assets/Script/EnemySpwaner.cs
+++ Assets/Script/EnemySpwaner.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class EnemySpwaner : MonoBehaviour {
 
+
+
+    GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
 }
