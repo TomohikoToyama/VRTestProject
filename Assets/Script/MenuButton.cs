c728dd98909a3c20b4512bb8261884560a0d1b18
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-07-27 23:55:57 +0900

    オブジェクトのnameでメニューボタンの何を対象にしたのか判別

--- Assets/Script/MenuButton.cs
+++ Assets/Script/MenuButton.cs
@@ -11,18 +11,45 @@ public class MenuButton : MonoBehaviour {
         Option,
         Quit,
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
 
-    public void GetMenuName()
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    public void test (){
+
+        if(GetMenuName() == MenuType.Start.ToString())
+        {
+            Debug.Log(GetMenuName());
+        }else if (GetMenuName() == MenuType.Practice.ToString())
+        {
+            Debug.Log(GetMenuName());
+        }else if (GetMenuName() == MenuType.Option.ToString())
+        {
+            Debug.Log(GetMenuName());
+        }else if (GetMenuName() == MenuType.Quit.ToString())
+        {
+            Debug.Log(GetMenuName());
+        }
+    }
+
+    //オブジェクト名を返す
+    public string GetMenuName()
     {
-        
+        return gameObject.name;   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Laser")
+        {
+            test();
+        }
     }
 }
