910c6d1914a3e793b69d2ab0325739f4b61ca5ce
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-08-14 23:59:48 +0900

    サウンド管理クラス追加

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VR
+{
+    public class SoundManager : MonoBehaviour
+    {
+        protected static SoundManager instance;
+
+        public static SoundManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (SoundManager)FindObjectOfType(typeof(SoundManager));
+
+                    if (instance == null)
+                    {
+                        Debug.LogError("SoundManager Instance Error");
+                    }
+                }
+
+                return instance;
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
