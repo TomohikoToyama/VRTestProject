3c12c8fe3f2f56c97779a7200a02cc7cc78ed96c
TomohikoToyama <golden.firework.launcher@gmail.com>
2018-09-18 23:59:51 +0900

    敵のオブジェクトマネージャー追加

---
+++ Assets/Script/EnemyObjectManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VR
+{
+    public class EnemyObjectManager : MonoBehaviour
+    {
+        protected static EnemyObjectManager instance;
+        public static EnemyObjectManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (EnemyObjectManager)FindObjectOfType(typeof(EnemyObjectManager));
+
+                    if (instance == null)
+                    {
+                        Debug.Log("MenuObjectManager Instance Error");
+                    }
+                }
+
+                return instance;
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
